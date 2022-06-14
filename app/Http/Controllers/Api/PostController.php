212a1c55c46a39b6830835f49febe82de9e3553c
larafenny <lara.filippone@gmail.com>
2022-06-14 22:46:10 +0200

    created function index in Api\Post controller + route

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
+
 
 class PostController extends Controller
 {
@@ -14,7 +16,11 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        //prendo tutti i post
+        $posts = Post::all();
+        //genero un json con i post
+        return response()->json($posts);
+
     }
 
     /**
